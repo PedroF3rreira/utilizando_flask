fe97ea1c36feaeabba5312f59e98960cee37db54
Pedro Daniel <pedro-edu19@outlook.com>
2023-03-29 11:50:29 -0300

    adicionando aquivos

--- application.py
+++ application.py
@@ -1,11 +1,59 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 
-app = Flask(__name__)
+app = Flask('framework_flask')
 
 
 @app.route("/")
 def ola():
     return u'Olá mundo! FLASK'
+
+@app.route("/usuarios")
+def index():
+    return u'usuario logado'
+
+
+#app.add_url_rule('/', endpoint='index', view_func='index')
+@app.route("/usuarios/lista")
+def lista_usuarios():
+    return u'Uma lista de usuarios'
+
+
+@app.route("/clientes/<function>")
+def clientes(function):
+    if function.lower() == "cadastro":
+        return u"Pagina de cadastro"
+    elif function.lower() == "lista":
+        return u"?Lista de usuarios"
+
+
+@app.route("/clientes/<int:id>")
+def atualiza_cliente(id):
+    return f"usuario {id}"
+
+@app.route("/admin")
+def ola_admin():
+    return u"Ola admin"
+
+
+@app.route("/visitante/<visitante>")
+def ola_visitante(visitante):
+    return f"Ola visitante {visitante}"
+
+
+@app.route("/usuario/<nome>")
+def ola_user(nome):
+    if nome.lower() == "admin":
+        return redirect(url_for('ola_admin'))
+    elif nome.lower() == "visitante":
+        return redirect(url_for('ola_visitante', visitante=nome))
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return u"Executar login do usuario"
+    elif request.method == 'GET':
+        return u"Formulário de login"
 
 
 if __name__ == "__main__":
---
+++ login.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, url_for, redirect, render_template
+
+app = Flask(__name__)
+
+# utilizando sistema de templates string jinja 2
+# função que apresenta pagina de boas vindas
+@app.route('/usuarios/<user_name>')
+def sucess(user_name):
+    return render_template('index.html', user_name=user_name, title="Pagina boas vindas")
+
+# função que faz login no sistema
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user_name = request.form['user_name']
+        return redirect(url_for('sucess', user_name=user_name))
+    else:
+        user_name = request.args.get('user_name')
+        return redirect(url_for('sucess', user_name=user_name))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
